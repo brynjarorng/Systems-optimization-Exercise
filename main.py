30c0088a652a532ff7115b26efb493b4a252ed27
brynjarorng <brynjarorng@gmail.com>
2021-09-07 11:51:20 +0200

    fix: using dicts, not classes

--- main.py
+++ main.py
@@ -1,33 +1,6 @@
 
 from bs4 import BeautifulSoup
 
-class Platform:
-    MCPs = []
-
-    def __init__(self, data):
-        mcps = data.find_all("MCP")
-
-        for i in mcps:
-            self.MCPs.append(MCP(i))
-
-class MCP:
-    MCP_ID = -1
-    CORES = []
-
-    def __init__(self, data):
-        self.MCP_ID = data.get("Id")
-        cores = data.find_all("Core")
-        for i in cores:
-            self.CORES.append({
-                "WCETFactor": i.get("WCETFactor"),
-                "Id": i.get("Id"),
-            })
-
-    def __str__(self):
-        return self.MCP_ID
-
- 
-
 def parser():
     with open('test_cases/small.xml', 'r') as f:
         data = f.read()
@@ -41,7 +14,7 @@ def parser():
     for mcp in mcp_list:
         cores = []
 
-        for core in mcp.get("Cores"):
+        for core in mcp.find_all("Core"):
             cores.append({
                 "Id": core.get("Id"),
                 "WCETFactor": core.get("WCETFactor")
@@ -67,7 +40,8 @@ def parser():
 
 
 
-    print(mcp_list)
+    print(mcps[0])
+    print(mcps[1])
 
            
 
