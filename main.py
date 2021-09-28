36927b608573d050d45fd9c1b1d209942f852ca4
brynjarorng <brynjarorng@gmail.com>
2021-09-28 11:12:24 +0200

    fix shit

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 import random
 import xml.etree.cElementTree as ET
 import math
+import copy
 
-SOLUTION_FILE = 'filename.xml'
+SOLUTION_FILE = 'large.xml'
+FILE_TO_READ = 'test_cases/large.xml'
 GLOBAL_OPTIMUM_SOLUTION = (None, -50000000000000000000000)
+MIN_TASKS_PER_CORE = 1
+SOLUTION_FOUND = False
 
 
 """
@@ -20,6 +24,15 @@ GLOBAL_OPTIMUM_SOLUTION = (None, -50000000000000000000000)
 """
 def set_initial_state(mcps, tasks):
     number_of_mcps = len(mcps)
+    avg_per_core = len(tasks) / sum( [ len(listElem["Cores"]) for listElem in mcps])
+
+    # Evenly distribute
+    for mcp in mcps:
+        for c in mcp["Cores"]:
+            for i in range(0, int(avg_per_core)):
+                c["TaskList"].append(tasks[0])
+                del tasks[0]
+
     number_of_cores = []
     for mcp in mcps:
         number_of_cores.append(len(mcp['Cores']))
@@ -95,13 +108,22 @@ def insert_random(mcps, task):
         The updated list of values
 """
 def move(num_to_move, mcps):
-    for i in range(num_to_move):
+    moves = 0;
+    while (moves < num_to_move):
         # Select random task and remove it
         mcp_index, core_index, task_index, task = get_random_task(mcps);
+
+        bool, _ = is_schedulable(mcps[mcp_index]['Cores'][core_index]['TaskList'], mcps[mcp_index]['Cores'][core_index]['WCETFactor'])
+        if bool:
+            moves += 1
+            continue
+
         del mcps[mcp_index]['Cores'][core_index]['TaskList'][task_index]
 
         # Insert task somewhere else
         mcps = insert_random(mcps, task)
+
+        moves += 1
         
     return mcps
 
@@ -121,6 +143,12 @@ def swap(swap_count, mcps):
     while (swaps < swap_count):
         mcp1_index, core1_index, task1_index, task1 = get_random_task(mcps);
         mcp2_index, core2_index, task2_index, task2 = get_random_task(mcps);
+
+        # bool, _ = is_schedulable(mcps[mcp1_index]['Cores'][core1_index]['TaskList'], mcps[mcp1_index]['Cores'][core1_index]['WCETFactor'])
+        # if bool:
+        #     swaps += 1
+        #     continue
+
         # Only do swaps if the two task are different
         if (task1 != task2):
             mcps[mcp1_index]['Cores'][core1_index]['TaskList'][task1_index] = task2;
@@ -152,19 +180,6 @@ def parse_solution(mcps):
 
     tree = ET.ElementTree(root)
     tree.write(SOLUTION_FILE)
-
-
-"""
-    Runs the main algorithm (Simulated Annealing) to find the best solution
-    
-    Args:
-        MPC list(main list)
-        
-    Returns:
-        best version of the MCP list
-"""
-def algorithm_sa():
-    pass
 
 
 """
@@ -206,7 +221,8 @@ def is_schedulable(tasks, core_factor):
         Laxity value
 """
 def laxity_calculator(mcps):
-    laxity, r = 0, 0
+    laxity = 0
+    r = 0
     deadlines = 0
     #sum(task['Deadline'] for task in tasks)
     for mcp in mcps:
@@ -216,7 +232,9 @@ def laxity_calculator(mcps):
                 deadlines += task['Deadline']
             bool, wcrts = is_schedulable(tasks, core["WCETFactor"])
             for w in wcrts:
-                r = r + w
+                r += w
+            if not bool:
+                r += deadlines * 209000
 
     laxity = deadlines - r
 
@@ -240,7 +258,6 @@ def parser(file_to_read):
     Bs_data = BeautifulSoup(data, "xml")
 
     # Parse the platform data
-    # platorm = Platform(Bs_data)
     mcps = []
     mcp_list = Bs_data.find_all("MCP")
     for mcp in mcp_list:
@@ -276,23 +293,27 @@ def parser(file_to_read):
     The simple annealing function 
 """
 def sa(mcps):
+    global SOLUTION_FOUND
     global GLOBAL_OPTIMUM_SOLUTION
-    swapper = [True, False]
-    T_max = 10000000000
+    switcher = [True, False]
+    T_max = 20000000000
     T = T_max
-    r = 0.00003
+    r = 0.0002
     laxity, _ = laxity_calculator(mcps)
     state_list = []
 
     while T > 1:
         # Generate neighbour using either swap or move
-        change_ammount = random.randrange(1, 6)
-        if random.choice(swapper):
-            mcps_new = swap(change_ammount, mcps)
+        change_ammount = random.randrange(5, 30)
+        if SOLUTION_FOUND:
+            if random.choice(switcher):
+                mcps_new = swap(1, copy.deepcopy(mcps))
+            else:
+                mcps_new = move(1, copy.deepcopy(mcps))
         else:
-            mcps_new = move(change_ammount, mcps)
+            mcps_new = move(change_ammount, copy.deepcopy(mcps))
 
-        new_laxity, _ = laxity_calculator(mcps_new)
+        new_laxity, deadline = laxity_calculator(mcps_new)
 
         if laxity < new_laxity or random.randrange(0, T_max) < T:
             # Set new state
@@ -301,7 +322,16 @@ def sa(mcps):
 
             # Set global best solution found so far
             if GLOBAL_OPTIMUM_SOLUTION[1] < laxity:
-                GLOBAL_OPTIMUM_SOLUTION = (mcps, laxity)
+                GLOBAL_OPTIMUM_SOLUTION = (copy.deepcopy(mcps), laxity, deadline)
+                print(laxity)
+                l = []
+                for i in GLOBAL_OPTIMUM_SOLUTION[0]:
+                    for c in i["Cores"]:
+                        l.append(is_schedulable(c['TaskList'], c["WCETFactor"])[0])
+                if all(l) is True:
+                    parse_solution(GLOBAL_OPTIMUM_SOLUTION[0])
+                    print(l)
+                    SOLUTION_FOUND = True
         
         T = T * (1 - r)
 
@@ -310,16 +340,18 @@ def sa(mcps):
 
 
 if __name__ == "__main__":
-    file_to_read = 'test_cases/small.xml'
-    mcps, tasks = parser(file_to_read)
+    mcps, tasks = parser(FILE_TO_READ)
+
     initial_state = set_initial_state(mcps, tasks)
 
-    results, mcps = sa(mcps) 
 
-    print(mcps)
-    print(results)
+    results, mcps = sa(mcps)
 
-    print(GLOBAL_OPTIMUM_SOLUTION)
+    print(GLOBAL_OPTIMUM_SOLUTION[1])
+    print(GLOBAL_OPTIMUM_SOLUTION[2])
 
+    for i in GLOBAL_OPTIMUM_SOLUTION[0]:
+        for c in i["Cores"]:
+            print(is_schedulable(c['TaskList'], c["WCETFactor"]))
 
-    # parse_solution = parse_solution(mcps)
+    parse_solution(GLOBAL_OPTIMUM_SOLUTION[0])
